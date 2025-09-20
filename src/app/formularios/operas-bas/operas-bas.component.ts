63c137d1fb5c5a6685f9ec452389dd685242339c
Matilde Blas <blasmati619@gmail.com>
2025-09-20 00:09:59 -0600

    operaciones basicas

--- src/app/formularios/operas-bas/operas-bas.component.ts
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -7,25 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './operas-bas.component.css'
 })
 export class OperasBasComponent {
-  num1: string = "";
-  num2: string = "";
-  res: string = "";
+
+  num1:string="";
+  num2:string="";
+  res:string="";
+  operacion:string="";
 
 
-sumar(): void {
-  this.res = (parseInt(this.num1) + parseInt(this.num2)).toString();
-}
-restar(): void {
-  this.res = (parseInt(this.num1) - parseInt(this.num2)).toString();
-}
-divi(): void {
-  this.res = (parseInt(this.num1) / parseInt(this.num2)).toString();
-}
-multi(): void {
-  this.res = (parseInt(this.num1) * parseInt(this.num2)).toString();
-}
-
-operaciones():void{
-  if (this.operaciones == '')
+  sumar():void{
+    this.res=(parseInt(this.num1) + parseInt(this.num2)).toString();
+  }
+  restar():void{
+    this.res=(parseInt(this.num1) - parseInt(this.num2)).toString();
+  }
+  multiplicar():void{
+    this.res=(parseInt(this.num1) * parseInt(this.num2)).toString();
+  }
+  dividir():void{
+    this.res=(parseInt(this.num1) / parseInt(this.num2)).toString();
+  }
+  operaciones():void{
+    if (this.operacion=="sumar"){
+      this.sumar();
+    }else if(this.operacion=="resta"){
+        this.restar();
+    }else if(this.operacion=="multiplicacion"){
+      this.multiplicar();
+    }else{
+      this.dividir();
+    }
+  }
 
 }
